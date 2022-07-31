92714b23c67aa0a3e7971fa22f0525cfd55c0b28
naufalfajri <muhammad.naufal@intern.bukalapak.com>
2022-07-31 14:18:00 +0700

    adding md5salt and create better main

--- main.py
+++ main.py
@@ -1,36 +1,67 @@
+from ast import If
 import aes
 import md5
+import md5salt
+import sha256
 import time
 
 def main():
-    plaintext = 'passwordpassword'
-    key = 'informatika12345'
-    iv = 'aesencryptionmd5'
+    # plaintext = 'passwordpassword'
+    # key = 'informatika12345'
+    # iv = 'aesencryptionmd5'
 
     print('==== AES >< MD5 ====')
-    
-    print('\nDoing AES Encryption')
+    plaintext = input("Insert plaintext: ")
     print('\nPlaintext : ', plaintext)
-
-    time1 = time.process_time()
-    for i in range(1000000):
-        ciphertext = aes.aes_enc(aes.pad(plaintext), key, iv)
-    time2 = time.process_time()
-
-    print('\nCiphertext : ', ciphertext)
-
-    print('\nDoing MD5 Hashing')
-    print('\nPlaintext : ', ciphertext)
+    option = int(input("\n1. MD5 + AES\n2. SHA256\n3. MD5 + salt\nChoose(1, 2 or 3): "))
     
-    time3 = time.process_time()
-    for i in range(1000000):
-        hash = md5.hashing(ciphertext)
-    time4 = time.process_time()
+    if option == 1 :
+        key = input("Insert key (key must be 16 char long): ")
+        print('\nKey : ', key)
+        iv = "aesencryptionmd5"
+        print('\nDoing AES Encryption')
 
-    print('\nHash : '+ hash + '\n')
+        time1 = time.process_time()
+        for i in range(1000000):
+            ciphertext = aes.aes_enc(aes.pad(plaintext), key, iv)
+        time2 = time.process_time()
 
-    time_elapsed = ((time2-time1)+(time4-time3))
-    print(f'Time taken to do the AES + MD5 process is : {time_elapsed}\n')
+        print('\nCiphertext : ', ciphertext)
+
+        print('\nDoing MD5 Hashing')
+        print('\nPlaintext : ', ciphertext)
+        
+        time3 = time.process_time()
+        for i in range(1000000):
+            hash = md5.hashing(ciphertext)
+        time4 = time.process_time()
+
+        print('\nHash : '+ hash + '\n')
+
+        time_elapsed = ((time2-time1)+(time4-time3))
+        print(f'Time taken to do the AES + MD5 process is : {time_elapsed/1000000}\n')
+    
+    elif option == 2:
+        time1 = time.process_time()
+        for i in range(1000000):
+            hash = sha256.hashing(plaintext)
+        time2 = time.process_time()
+
+        print('\nHash : '+ hash + '\n')
+
+        time_elapsed = time2-time1
+        print(f'Time taken to do the SHA256 process is : {time_elapsed/1000000}\n')
+    
+    elif option == 3:
+        time1 = time.process_time()
+        for i in range(1000000):
+            hash = md5salt.hashing(plaintext)
+        time2 = time.process_time()
+
+        print('\nHash : '+ hash + '\n')
+        
+        time_elapsed = time2-time1
+        print(f'Time taken to do the SHA256 process is : {time_elapsed/1000000}\n')
 
 if __name__ == '__main__':
     main()
---
+++ md5salt.py
@@ -0,0 +1,26 @@
+from hashlib import md5
+import secrets
+import string
+
+def hashing(plain_text):
+    # generate 8 byte random salt and add it to plaintext
+    salt = ''.join(secrets.choice(string.ascii_letters + string.digits) for i in range(8))
+    plain_text = plain_text + salt
+    # encode the plaintext
+    plain_text = plain_text.encode()
+    
+    # do the md5 hashing
+    hash = md5(plain_text)
+
+    # return the hex value
+    return hash.hexdigest()
+
+def main():
+    plaintext = "oya jelek"
+    hash = hashing(plaintext)
+
+    print(plaintext)
+    print(hash)
+
+if __name__ == "__main__":
+    main()
